2ed79333e38f86b21d591a4cacdd2df92ef66498
Junfeng_Lu <154610185+kikjhhsgdwxa@users.noreply.github.com>
2024-02-08 20:27:24 +0800

    Delete Circle directory

--- Circle/API/API/TestAPI.java
+++
@@ -1,4 +0,0 @@
-package Circle.API.API;
-public interface TestAPI {
-    void Test();
-}
--- Circle/API/PI/CirclePiusAPI.java
+++
@@ -1,8 +0,0 @@
-package Circle.API.PI;
-public interface CirclePiusAPI {
-    float PAI = 3.14159F;
-    void getArea();
-    void getGirth();
-    void draw();
-    void ReadR();
-}
